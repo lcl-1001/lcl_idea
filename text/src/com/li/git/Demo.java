21677b763d89366433ed4cac07e62ef09d545d04
lcl-1001 <2239583776@qq.com>
2019-10-17 22:58:23 +0800

    git send local repository

---
+++ text/src/com/li/git/Demo.java
@@ -0,0 +1,13 @@
+package com.li.git;
+
+/**
+ * @author dragon
+ * @site
+ * @company
+ * @create  2019-10-17 22:54
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("sdahufiabndjakdvn ");
+    }
+}
